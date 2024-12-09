5023e06eeb92f0b9934427657bad80933b7be90e
Denis <IspiritI@mail.ru>
2024-12-09 19:41:34 +0500

    1

---
+++ src/Den1/one2/App.java
@@ -0,0 +1,24 @@
+package Den1.one2;
+
+import Den1.two2.Hello;
+
+import static Den1.two2.Dz.*;
+
+
+public class App {
+    static String s = "World";
+    static String q = "112";
+    static String one = q.substring(2);
+    static String a = "321";
+    static Integer two = Integer.parseInt(a.substring(0,1)) + Integer.parseInt(a.substring(1,2)) + Integer.parseInt(a.substring(2,3));
+    public static void main(String[] args) {
+        Hello.helloPrint(s);
+        System.out.println(one);
+        System.out.println(two);
+    //    plusPrint();
+    //    plusMinusDes();
+    //    minimumInt();
+        chetNechet();
+    }
+}
+
---
+++ src/Den1/two2/Hello.java
@@ -0,0 +1,8 @@
+package Den1.two2;
+
+public abstract class Hello {
+    public static void helloPrint(String s)
+    {
+        System.out.println("Hello " + s);
+    }
+}
